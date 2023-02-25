30f3c04a0b57dddd978303050bbbd471631a653f
RedGuy12 <paul@reid-family.org>
2023-02-25 13:26:02 -0600

    QWIR-30: Complete pan and zoom functionality
    Signed-off-by: RedGuy12 <paul@reid-family.org>

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,19 +1,25 @@
 <template>
-	<section id="board" v-dragscroll>
-		<div class="row" v-for="rowIndex in boardSize.rows[1] - boardSize.rows[0] + 3">
-			<div
-				class="tile"
-				v-for="columnIndex in boardSize.columns[1] - boardSize.columns[0] + 3"
-				@click="tilePlaced"
-			>
-				<img
-					v-if="getFromBoard(columnIndex, rowIndex)"
-					:src="generateTileUrl(getFromBoard(columnIndex, rowIndex))"
-					:alt="`${getFromBoard(columnIndex, rowIndex)?.color} ${
-						getFromBoard(columnIndex, rowIndex)?.shape
-					}`"
-				/>
+	<section id="board" v-dragscroll :style="{ '--scale': scale }">
+		<div id="rows">
+			<div class="row" v-for="rowIndex in boardSize.rows[1] - boardSize.rows[0] + 3">
+				<div
+					class="tile"
+					v-for="columnIndex in boardSize.columns[1] - boardSize.columns[0] + 3"
+					@click="tilePlaced"
+				>
+					<img
+						v-if="getFromBoard(columnIndex, rowIndex)"
+						:src="generateTileUrl(getFromBoard(columnIndex, rowIndex))"
+						:alt="`${getFromBoard(columnIndex, rowIndex)?.color} ${
+							getFromBoard(columnIndex, rowIndex)?.shape
+						}`"
+					/>
+				</div>
 			</div>
+		</div>
+		<div id="zoom">
+			<button :disabled="scale === SCALE_BOUNDS[1]" @click="zoomIn"><span>+</span></button>
+			<button :disabled="scale === SCALE_BOUNDS[0]" @click="zoomOut"><span>-</span></button>
 		</div>
 	</section>
 	<section id="hand">
@@ -44,6 +50,9 @@
 		};
 		placedTiles: Record<Location["y"], Record<Location["x"], PlacedTile>> = {};
 		selectedTile = -1;
+		scale = 1;
+		SCALE_BOUNDS = [0.35, 1.5] as const;
+		SCALE_INCREMENT = 0.05 as const;
 
 		// Refs
 		declare readonly $refs: {};
@@ -115,6 +124,12 @@
 			const locations = this.parseRawIndexes(rawColumn, rawRow);
 			return this.placedTiles[locations.row]?.[locations.column];
 		}
+		zoomIn() {
+			this.scale = Math.min(this.SCALE_BOUNDS[1], this.scale + this.SCALE_INCREMENT);
+		}
+		zoomOut() {
+			this.scale = Math.max(this.SCALE_BOUNDS[0], this.scale - this.SCALE_INCREMENT);
+		}
 	}
 </script>
 <style scoped>
@@ -122,29 +137,60 @@
 		height: calc(100% - 265px);
 		overflow: auto;
 		border: 1px solid #000;
-		width: 100%;
 		margin-top: 20px;
+		scrollbar-width: none;
+		--cursor: grab;
+		cursor: var(--cursor);
+
 		display: flex;
-		flex-direction: column;
-		flex-wrap: wrap;
-		justify-content: center;
+		justify-content: flex-start;
+		align-items: flex-start;
+	}
+
+	#board::-webkit-scrollbar {
+		display: none;
+	}
+
+	#rows {
+		margin: auto;
 	}
 
 	.row {
 		display: flex;
-		width: min-content;
-		margin: 0 auto;
 	}
 
 	.tile {
-		width: 100px;
-		height: 100px;
+		height: calc(100px * var(--scale));
+		width: calc(100px * var(--scale));
 		display: inline-block;
 		cursor: pointer;
+		border: 1px solid;
 	}
 
 	.tile img {
-		cursor: not-allowed;
+		width: 100%; /* TODO: higher quality images */
+		cursor: var(--cursor);
+	}
+
+	#zoom {
+		bottom: 0;
+		position: absolute;
+		right: 0;
+	}
+
+	#zoom button {
+		border-radius: 100%;
+		font-size: 40px;
+		height: 50px;
+		width: 50px;
+		align-content: center;
+		justify-content: center;
+		margin: 10px;
+	}
+
+	#zoom button span {
+		height: 10.5px;
+		line-height: 0;
 	}
 
 	#hand {
@@ -160,6 +206,7 @@
 		margin: 5px;
 		padding: 0;
 		width: 125px;
+		background: #eee;
 	}
 
 	.selected {
@@ -169,5 +216,6 @@
 	.selected img {
 		box-shadow: 0 0 5px 0 #000000;
 		height: 100px;
+		margin: auto;
 	}
 </style>
