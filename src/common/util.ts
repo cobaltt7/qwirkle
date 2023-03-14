16514ad83b354d92028d90de278710aa56f65902
RedGuy12 <paul@reid-family.org>
2023-03-14 12:00:18 -0500

    Today for coding, I moved a function and its dependencies to a new module in preparation for QWIR-21.
    Signed-off-by: RedGuy12 <paul@reid-family.org>

--- src/common/util.ts
+++ src/common/util.ts
@@ -1,4 +1,5 @@
-import type { JWTClaims } from "./types.js";
+import { TILE_COLORS, TILE_SHAPES } from "./constants.js";
+import type { Board, JWTClaims, PlacedTile, PlaceError, Rooms, Tile } from "./types.js";
 
 export function getUsername() {
 	const jwt = localStorage.getItem("auth");
@@ -6,4 +7,101 @@ export function getUsername() {
 	const encoded = jwt?.split(".")[1];
 	if (!encoded) return null;
 	return (JSON.parse(window.atob(encoded)) as JWTClaims).username;
+}
+
+export function placeTile(tile: PlacedTile, board: Board): PlaceError | undefined {
+	const row = board[tile.y] ?? {};
+	if (row[tile.x]) return "ALREADY_PLACED";
+
+	let top = board[tile.y - 1]?.[tile.x],
+		bottom = board[tile.y + 1]?.[tile.x],
+		right = row[tile.x + 1],
+		left = row[tile.x - 1];
+	if (!top && !bottom && !right && !left) return "NO_NEIGHBORS";
+
+	const neighborhood: { row: PlacedTile[]; column: PlacedTile[] } = { row: [], column: [] };
+	if (right || left) {
+		if (left) {
+			do neighborhood.row.unshift(left);
+			while ((left = row[left.x - 1]));
+		}
+		neighborhood.row.push(tile);
+		if (right) {
+			do neighborhood.row.push(right);
+			while ((right = row[right.x + 1]));
+		}
+	}
+	if (top || bottom) {
+		if (top) {
+			do neighborhood.column.unshift(top);
+			while ((top = board[top.y - 1]?.[tile.x]));
+		}
+		neighborhood.column.push(tile);
+		if (bottom) {
+			do neighborhood.column.push(bottom);
+			while ((bottom = board[bottom.y + 1]?.[tile.x]));
+		}
+	}
+
+	const rowResult = verifyLine(neighborhood.row);
+	if (rowResult) return `${rowResult}_ROW_ITEMS`;
+	const columnResult = verifyLine(neighborhood.column);
+	if (columnResult) return `${columnResult}_COLUMN_ITEMS`;
+}
+
+export function getRandomTile(deck: Tile[], required: true): Tile;
+export function getRandomTile(deck: Tile[], required?: false): Tile | undefined;
+export function getRandomTile(deck: Tile[], required = false): Tile | undefined {
+	const index = Math.floor(Math.random() * deck.length);
+	const tile = deck[index];
+	if (!tile && required) throw new RangeError("Deck is empty");
+	deck.splice(index, 1);
+	return tile;
+}
+
+export function generateHand(deck: Tile[]) {
+	const hand = [];
+	while (deck.length > 0 && hand.length < 6) {
+		hand.push(getRandomTile(deck, true));
+	}
+	return sortHand(hand);
+}
+
+export function sortHand(hand: Tile[]) {
+	return hand.sort(
+		(one, two) =>
+			TILE_COLORS.indexOf(one.color) - TILE_COLORS.indexOf(two.color) ||
+			TILE_SHAPES.indexOf(one.shape) - TILE_SHAPES.indexOf(two.shape),
+	);
+}
+
+export function verifyLine(line: PlacedTile[]) {
+	const isSameColor = line.every((tile) => tile.color === line[0]?.color);
+	const isSameShape = line.every((tile) => tile.shape === line[0]?.shape);
+	if (!(isSameColor || isSameShape)) return "INCONSISTENT";
+
+	return (
+		line.some((tile, index) => {
+			const key = isSameColor ? tile.shape : tile.color;
+			return (
+				line.findIndex((item) => item[isSameColor ? "shape" : "color"] === key) !== index
+			);
+		}) && "DUPLICATE"
+	);
+}
+
+export function getPublicRooms(rooms: Rooms) {
+	return Object.fromEntries(
+		Object.entries(rooms).filter(([, room]) => !room.private && !room.started),
+	);
+}
+
+export function generateRoomId() {
+	const factor = 6;
+	const power = 10 ** factor;
+	const dateSalt = Date.now() % power;
+	return (
+		Math.floor(Math.random() * power + dateSalt).toString(36) +
+		Math.floor(Math.random() * power + dateSalt).toString(36)
+	).substring(0, 8);
 }
--- src/server/socket.ts
+++ src/server/socket.ts
@@ -1,10 +1,17 @@
 import { Server as SocketServer } from "socket.io";
 import type { Server as HTTPServer } from "node:http";
 import { TILE_COLORS, TILE_SHAPES } from "../common/constants.js";
+import {
+	getPublicRooms,
+	generateRoomId,
+	placeTile,
+	generateHand,
+	getRandomTile,
+	sortHand,
+} from "../common/util.js";
 import type {
 	ClientToServerEvents,
 	InterServerEvents,
-	PlacedTile,
 	Rooms,
 	ServerToClientEvents,
 	SocketData,
@@ -35,7 +42,7 @@ export default function connectIo(server: HTTPServer) {
 	io.on("connection", (socket) => {
 		socket
 			.once("mounted", () => {
-				socket.emit("roomsUpdate", getPublicRooms());
+				socket.emit("roomsUpdate", getPublicRooms(rooms));
 				const roomId = [...socket.rooms.values()][1];
 				if (!roomId) return;
 				const room = rooms[roomId];
@@ -60,7 +67,7 @@ export default function connectIo(server: HTTPServer) {
 				room.players.push(roomData.username);
 				rooms[roomId] = room;
 
-				io.emit("roomsUpdate", getPublicRooms());
+				io.emit("roomsUpdate", getPublicRooms(rooms));
 				io.to(roomId).emit("playersUpdate", room.players);
 
 				const jwt = await new SignJWT({ username: roomData.username } as JWTClaims)
@@ -86,7 +93,7 @@ export default function connectIo(server: HTTPServer) {
 				if (room.started) return callback("ALREADY_STARTED");
 				socket.join(roomId);
 				callback();
-				io.emit("roomsUpdate", getPublicRooms());
+				io.emit("roomsUpdate", getPublicRooms(rooms));
 				io.to(roomId).emit("playersUpdate", room.players);
 			})
 			.on("startGame", async (callback) => {
@@ -116,65 +123,18 @@ export default function connectIo(server: HTTPServer) {
 				if (!roomId) return callback("NOT_IN_ROOM");
 				const room = rooms[roomId];
 				if (!room) return callback("UNDEFINED_ROOM");
-				// STEP 1: Gather base information.
+
 				const hand = hands[socket.id] ?? generateHand(room.deck);
 				const tile = hand[Number(index)];
 				if (!tile) return callback("MISSING_TILE");
+
 				const placed = { ...tile, ...location };
+				const error = placeTile(placed, room.board);
+				if (error) return callback(error);
 
-				// STEP 2: Check for collisions.
-				const row = room.board[Number(location.y)] ?? {};
-				if (row[location.x]) return callback("ALREADY_PLACED");
-
-				// STEP 3: Check for neighbors.
-				let top = room.board[location.y - 1]?.[Number(location.x)],
-					bottom = room.board[location.y + 1]?.[Number(location.x)],
-					right = row[location.x + 1],
-					left = row[location.x - 1];
-				if (!top && !bottom && !right && !left) return callback("NO_NEIGHBORS");
-
-				// STEP 4: Check neighborhood to verify consistency.
-				const neighborhood: { row: PlacedTile[]; column: PlacedTile[] } = {
-					row: [],
-					column: [],
-				};
-				//Step 4.0-4.3: Get neighborhood
-				if (right || left) {
-					if (left) {
-						do neighborhood.row.unshift(left);
-						while ((left = row[left.x - 1]));
-					}
-					neighborhood.row.push(placed);
-					if (right) {
-						do neighborhood.row.push(right);
-						while ((right = row[right.x + 1]));
-					}
-				}
-				if (top || bottom) {
-					if (top) {
-						do neighborhood.column.unshift(top);
-						while ((top = room.board[top.y - 1]?.[Number(location.x)]));
-					}
-					neighborhood.column.push(placed);
-					if (bottom) {
-						do neighborhood.column.push(bottom);
-						while ((bottom = room.board[bottom.y + 1]?.[Number(location.x)]));
-					}
-				}
-				// Step 4.4: Check that the color OR the shape of each row item is the same
-				// Step 4.5: Check that the other has no duplicates
-				// Steps 4.6-4.7: Repeat for columns
-				const rowResult = verifyLine(neighborhood.row);
-				if (rowResult) return callback(`${rowResult}_ROW_ITEMS`);
-				const columnResult = verifyLine(neighborhood.column);
-				if (columnResult) return callback(`${columnResult}_COLUMN_ITEMS`);
-
-				// STEP 5: Place the tile.
-				row[Number(location.x)] = placed;
-				room.board[Number(location.y)] = row;
+				(room.board[placed.y] ??= {})[placed.x] = placed;
 				io.to(roomId).emit("tilePlaced", placed);
 
-				// STEP 6: Update the user's hand.
 				const newTile = getRandomTile(room.deck);
 				if (newTile) hand[Number(index)] = newTile;
 				else hand.splice(index, 1);
@@ -184,61 +144,4 @@ export default function connectIo(server: HTTPServer) {
 				// TODO
 			});
 	});
-}
-
-function getRandomTile(deck: Tile[], required: true): Tile;
-function getRandomTile(deck: Tile[], required?: false): Tile | undefined;
-function getRandomTile(deck: Tile[], required = false): Tile | undefined {
-	const index = Math.floor(Math.random() * deck.length);
-	const tile = deck[index];
-	if (!tile && required) throw new RangeError("Deck is empty");
-	deck.splice(index, 1);
-	return tile;
-}
-
-function generateHand(deck: Tile[]) {
-	const hand = [];
-	while (deck.length > 0 && hand.length < 6) {
-		hand.push(getRandomTile(deck, true));
-	}
-	return sortHand(hand);
-}
-
-function sortHand(hand: Tile[]) {
-	return hand.sort(
-		(one, two) =>
-			TILE_COLORS.indexOf(one.color) - TILE_COLORS.indexOf(two.color) ||
-			TILE_SHAPES.indexOf(one.shape) - TILE_SHAPES.indexOf(two.shape),
-	);
-}
-
-function verifyLine(line: PlacedTile[]) {
-	const isSameColor = line.every((tile) => tile.color === line[0]?.color);
-	const isSameShape = line.every((tile) => tile.shape === line[0]?.shape);
-	if (!(isSameColor || isSameShape)) return "INCONSISTENT";
-
-	return (
-		line.some((tile, index) => {
-			const key = isSameColor ? tile.shape : tile.color;
-			return (
-				line.findIndex((item) => item[isSameColor ? "shape" : "color"] === key) !== index
-			);
-		}) && "DUPLICATE"
-	);
-}
-
-function getPublicRooms() {
-	return Object.fromEntries(
-		Object.entries(rooms).filter(([, room]) => !room.private && !room.started),
-	);
-}
-
-function generateRoomId() {
-	const factor = 6;
-	const power = 10 ** factor;
-	const dateSalt = Date.now() % power;
-	return (
-		Math.floor(Math.random() * power + dateSalt).toString(36) +
-		Math.floor(Math.random() * power + dateSalt).toString(36)
-	).substring(0, 8);
 }
